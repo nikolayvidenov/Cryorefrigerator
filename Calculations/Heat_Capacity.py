eb7239e08834a4a682319fe8198513c406e87ed4
Nikolay-Videnov <nnvideno@uwaterloo.ca>
2018-10-22 06:58:32 -0400

    thermal calculations

--- Calculations/Heat_Capacity.py
+++ Calculations/Heat_Capacity.py
@@ -1,136 +1,47 @@
 # coding: utf-8
 
-import sympy
-import numpy
-import scipy
 import scipy.integrate as integrate
 from scipy.constants import *
-th_d = h*c/(2*np.pi*k)*(6*pi**2*(NV))**(1/3.)
 import numpy as np
+
 copper_th_d = 343
 aluminium_th_d = 428
-c = lambda x: ( x**4.*np.exp(x) ) / ( np.exp(x)-1)**2
-r = integrate.quad(c, 0, copper_th_d/300)
-r
-r[0]
-r[0]*9* N_A*k *300**4/copper_th_d**3
-x = np.linspace(4, 300, 16)
-x
-x = np.linspace(4, 300, 14)
-x
-x = np.linspace(4, 300, 15)
-def c(T):
-    r = integrate.quad(c, 0, copper_th_d/T)
-    return r[0]*9*N_A*k*(T/ copper_th_d)**3
-N_A
-k
-y = np.array(map(c, x))
-def c(T):
-    print T
-    r = integrate.quad(c, 0, copper_th_d/T)
-    return r[0]*9*N_A*k*(T/ copper_th_d)**3
-y = np.array(map(c, x))
-for i in x:
-    print i
-    
-y = []
-for i in x:
-    print i
-    y.append(c(i))
-    
-: c = lambda x: ( x**4.*np.exp(x) ) / ( np.exp(x)-1)**2
+# Note that the units are per mol for the heat capacity
+# So you must divide by the molar mass to get the heat
+# capacity per mass and again to get to kilograms
+copper_molar_mass = 0.063 # Kg/mol
+aluminium_molar_mass = 0.027 # Kg/mol
 
-In [11]: r = integrate.quad(c, 0, copper_th_d/300)
-: c = lambda x: ( x**4.*np.exp(x) ) / ( np.exp(x)-1)**2
 c = lambda x: ( x**4.*np.exp(x) ) / ( np.exp(x)-1)**2
-def C(T):
-    print T
-    r = integrate.quad(c, 0, copper_th_d/T)
-    return r[0]*9*N_A*k*(T/ copper_th_d)**3
-y = np.array(map(C, x))
-y
-x = np.linspace(4, 300, 25)
-x
-x = np.linspace(4, 300, 26)
-x
-x
-y
-C(10)
-copper_th_d = 343.
-C(10)
-c = lambda x: ( x**4.*np.exp(x) ) / ( np.exp(x)-1)**2
-y /= 0.063
-y
-y = np.array(map(C, x))
-y
-def C(T):
-    print T
-    r = integrate.quad(c, 0, copper_th_d/T)
-    return r[0]*9*N_A*k*(T/ copper_th_d)**3/0.063
-y = np.array(map(C, x))
-y
-C(100)
-def C(T):
-    print T
-    r = integrate.quad(c, 0, copper_th_d/T)
-    return r[0]*9*N_A*k*(T/ copper_th_d)**3
-C(100)
-C(300)
-c = lambda x: ( x**4.*np.exp(x) ) / ( np.exp(x)-1)**2
-C(1000)
-C(100000)
-3*N_A*k
-C(1)
-R 
-N_A*K
-N_A*k
-12*np.pi**4/5
-12*np.pi**4/5*(1/ copper_th_d)**3
-12*np.pi**4/5*(1/ copper_th_d)**3*R
-15/0.06
-25/0.06
-x = [4
-6
-8
-10
-12
-14
-16
-18
-20
-30
-40
-50
-60
-70
-80
-90
-100
-120
-140
-160
-180
-200
-220
-240
-260
-280
-300
-]
-x=[4, 6, 8, 10, 12, 14, 16, 18, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]
-y = np.array(map(C, x))
-y
+
 def C_Al(T):
-    print T
-    r = integrate.quad(c, 0, aluminium_th_d /T)
-    return r[0]*9*N_A*k*(T/ aluminium_th_d)**3
-def C_Al(T):
-    print T
-    r = integrate.quad(c, 0, aluminium_th_d /T)
-    return r[0]*9*N_A*k*(T/ aluminium_th_d)**3/0.027
-y = np.array(map(C_Al, x))
-y
-aluminium_th_d = 428.
-y = np.array(map(C_Al, x))
-u
-y
+    r = integrate.quad(c, 0, aluminium_th_d/T)
+    return 9*R*(T / aluminium_th_d)**3*r[0]/aluminium_molar_mass
+
+def C_Cu(T):
+    r = integrate.quad(c, 0, copper_th_d/T)
+    return 9*R*(T / copper_th_d)**3*r[0]/copper_molar_mass
+
+x = np.linspace(50, 300, 500)
+
+# cooling power as a function of temperature from matching
+# the PT415 data in the excel spreadhseet
+
+def cool_power(T):
+    return 15.6478521420156*(T - 31.3432835820895)**.5
+
+def cool_time_Al(T):
+    return C_Al(T) / cool_power(T)
+
+def cool_time_Cu(T):
+    return C_Cu(T) / cool_power(T)
+
+# cool time per kilogram of aluminum
+Al_time = integrate.quad(cool_time_Al, 40, 300)[0]
+
+# cool time per kilogram of copper
+Cu_time = integrate.quad(cool_time_Cu, 40, 300)[0]
+
+print("Al: ", Al_time/60, "min/Kg")
+print("Cu: ", Cu_time/60, "min/Kg")
+
